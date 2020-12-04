b431e2a641fc340151ba6fe5a2205e5ded5d0554
Damilola Orunsholu <damilolaorunsholu@gmail.com>
2020-12-04 11:00:02 +0000

    Initial commit

---
+++ 4.6.py
@@ -0,0 +1,12 @@
+def computepay(h,r):
+    x=0
+    if h > 40.0:
+     x = r * 40.0 +(1.5*r*(h-40.0))
+    elif h <= 40 :
+   	 x= h*r
+    return x
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter the rate:"))
+p = computepay(hrs,rate)
+print(p)
